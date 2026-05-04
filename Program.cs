5c7e57951019a86cde491d1c7ab4e3f73c1b0c61
ivanp <ivanp@IVPC>
2026-05-04 18:03:45 +0100

    AAA

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Modifiquei o programa - 2");
+            Console.WriteLine("Modifiquei o programa - 3");
         }
     }
 }
